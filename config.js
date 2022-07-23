56b40aa394496cf96767109c88825867c2299a57
xyhaisha <109809696+xyhaisha@users.noreply.github.com>
2022-07-23 14:07:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'xyhaisha', // 网站标题
-  nickName: "xyhaisha", // 昵称
+  title: '自由的创作', // 网站标题
+  nickName: "云与原", // 昵称
   githubName: 'xyhaisha', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '书写值得书写的东西',
+  SEO_description: '自由的的博客，自由的创作',
   SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
